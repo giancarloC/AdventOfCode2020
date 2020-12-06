5e9107506d95179fe29310d1551412e8ea9b6d66
giancarloC <gcalle288@gmail.com>
2020-12-06 00:33:53 -0500

    finished day 6

---
+++ day6/src/part1.rs
@@ -0,0 +1,42 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashSet;
+
+
+pub fn get_result(){
+    match find_sum_count() {
+        Ok(num) => println!("Sum of the counts are: {}", num),
+        Err(_e) => println!("Incorrect Input"),
+    }
+}
+
+//counts number of passports
+fn find_sum_count() -> io::Result<i32> {
+    let f = File::open("./input/input.txt")?;
+    let f = BufReader::new(f);
+
+    let mut count = 0;
+    let mut questions: HashSet<char> = HashSet::new();
+
+    //finds max seat
+    for line in f.lines(){
+        let string = line.unwrap();
+
+        if string == ""{
+            count += questions.len() as i32;
+            questions.clear();
+        }
+        
+        else{
+            for c in string.chars(){
+                questions.insert(c);
+            }
+        }
+    }
+
+    //adds last one
+    count += questions.len() as i32;
+  
+    return Ok(count);
+}
---
+++ day6/src/part2.rs
@@ -0,0 +1,61 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
+
+
+pub fn get_result(){
+    match find_sum_count() {
+        Ok(num) => println!("Sum of the counts are: {}", num),
+        Err(_e) => println!("Incorrect Input"),
+    }
+}
+
+//counts number of passports
+fn find_sum_count() -> io::Result<i32> {
+    let f = File::open("./input/input.txt")?;
+    let f = BufReader::new(f);
+
+    let mut count = 0;
+    let mut group_count = 0;
+    let mut questions: HashMap<char, i32> = HashMap::new();
+
+    //finds max seat
+    for line in f.lines(){
+        let string = line.unwrap();
+
+        if string == ""{
+            //loops through map
+            for (key, val) in questions.iter(){
+                if val == &group_count{
+                    count += 1;
+                }
+            }
+
+            questions.clear();
+            group_count = 0;
+        }
+        
+        else{
+            group_count += 1;
+            
+            for c in string.chars(){
+                if questions.contains_key(&c){
+                    questions.insert(c, questions.get(&c).unwrap() + 1);
+                }
+                else{
+                    questions.insert(c, 1);
+                }
+            }    
+        }
+    }
+
+    //loops through last one
+    for (key, val) in questions.iter(){
+        if val == &group_count{
+            count += 1;
+        }
+    }
+  
+    return Ok(count);
+}
